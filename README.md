83c1ef6a83e251974f3aca69b7941f0e81b65040
Tinque <quentin@beekast.com>
2017-07-03 17:27:11 +0200

    migrate function request to client and use a unique responseQueue for all the service

--- README.md
+++ README.md
@@ -63,8 +63,18 @@ Return a new RabbitmqRPC client.
  * `reconnectDelay` : Delay in ms before reconnect on connection close (default `1000`)
  * `autoReconnect` : Enable autoReconnect option (default `true`)
 
-### {client} createService(options)
+
+### {client} request(serviceName, method,data)
+Request a method on a service.
+`serviceName` string for the service name.
+`method` is the method name, you have to use alphanum string for the method name.
+`data` is the data send to the client.
+
+This function return a Promise.
+
+### {client} createService(serviceName, options)
 Return a service object
+`serviceName` string for the service name.
 `options` are :
 * `autoStartConsume` : If the service start to consume RPC message at the beginning (default : `false`)
 * `replyTimeout` : Timeout for rpc request in ms (default : `2000`)
@@ -76,13 +86,6 @@ Handle a method for the service.
 `function` is the function called. It can be a function or a promise. The return of the function is return to the client. The function take one arg whose is the data send by the client.
 
 > !IMPORTANT! - if you don't use the option autoStartConsume: true on service constructor don't forget to call service.startConsume() to handle the RabbitmqRPC message.
-
-### {service} request(method,data)
-Request a method on a service.
-`method` is the method name, you have to use alphanum string for the method name.
-`data` is the data send to the client.
-
-This function return a Promise.
 
 ## Contributing
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const Connection = require('./connection');
 const Logger = require('./logger');
 const Service = require('./service');
 
+const uuidV4 = require('uuid/v4');
+
 class RabbitmqRPC {
 
 	constructor (opts){
@@ -10,11 +12,14 @@ class RabbitmqRPC {
 			logLevel = 'info',
 			logName = 'RabbitmqRPC',
 			exchangeName = 'RabbitmqRPC',
+			responseQueue = true,
 			reconnectDelay = 1000,
 			autoReconnect = true,
+			replyTimeout = 2000,
 			log
 		} = opts || {};
 
+		this._replyTimeout = replyTimeout;
 		this._url = url;
 		this._log = log || Logger({
 			level: logLevel,
@@ -27,6 +32,117 @@ class RabbitmqRPC {
 			exchangeName,
 			reconnectDelay,
 			autoReconnect
+		});
+
+		// autoReconnect exchange;
+		if (this._connection.autoReconnect){
+			// on channel close restart consumer and responseQueue
+			this._connection.on('close', () => {
+				this._reconnect();
+			});
+		}
+
+		this._requests = [];
+		if (responseQueue){
+			if (typeof responseQueue === 'boolean'){
+				this._responseQueue = this.exchangeName + '-responseQueue-'+uuidV4();
+			} else {
+				this._responseQueue = responseQueue;
+			}
+			this.createResponseQueue();
+		}
+		this._requestChannel = this._connection.getChannel();
+
+	}
+
+	_reconnect (){
+		return setTimeout(() => {
+			this._log.info('reconnect service');
+			this._requestChannel = this._connection.getChannel();
+			if (this._responseQueue){
+				this.createResponseQueue();
+			}
+		}, this._connection.reconnectDelay);
+	}
+
+	createResponseQueue (){
+		if (this.createResponseQueuePromise){
+			return this.createResponseQueuePromise;
+		} else {
+			if (this._responseQueue){
+				// force to have a responseQueue name in case of request without responseQueue
+				this._responseQueue = this.exchangeName + '-responseQueue-'+uuidV4();
+			}
+
+			this.createResponseQueuePromise = this._connection.getChannel()
+			.then((channel) => {
+				return channel.assertQueue(this._responseQueue, {durable: false, exclusive: true, autoDelete: true})
+				.then(({queue}) => {
+					return channel.consume(queue, (message) => {
+						const requestId = message.properties.correlationId;
+						const {
+							err,
+							data
+						} = JSON.parse(message.content.toString());
+						if (requestId && this._requests[requestId]){
+							this._requests[requestId](err, data);
+						}
+						channel.ack(message);
+					});
+				});
+			});
+			return this.createResponseQueuePromise;
+		}
+	}
+
+	request (serviceName, method, data) {
+		const requestId = uuidV4();
+
+		const content = JSON.stringify(data);
+
+		return new Promise((resolve, reject) => {
+
+			const timeout = setTimeout( () => {
+				delete this._requests[requestId];
+				return reject( new Error( 'No reply received within the configured timeout of ' + this._replyTimeout + ' ms' ) );
+			}, this._replyTimeout );
+
+
+			this._requests[requestId] = (err, data) => {
+				delete this._requests[requestId];
+				clearTimeout( timeout );
+				if (err){
+					return reject(new Error(err));
+				}
+				return resolve(data);
+			};
+
+			const bufferContent = new Buffer(content);
+
+			Promise.all([
+				this._connection.createExchange(),
+				this.createResponseQueue()
+			])
+			.then(() => {
+				this._requestChannel
+				.then((channel) => {
+					channel.publish(
+						this._connection.exchangeName,
+						serviceName,
+						bufferContent,
+						{
+							expiration: this._replyTimeout,
+							correlationId: requestId,
+							replyTo: this._responseQueue,
+							type: method
+						}
+					);
+				});
+			})
+			.catch((err) => {
+				this._log.error(err);
+				return reject(err);
+			});
 		});
 	}
 
